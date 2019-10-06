236cb148b1d4484416f5b5fe52805204bd477f76
Aaron Vega <aaroncvega123@gmail.com>
2019-10-06 15:54:34 -0700

    Removed unused TestView
    TestView's purpose is now used by get_test_view

--- MessageBoard/message_board_site/views.py
+++ MessageBoard/message_board_site/views.py
@@ -6,16 +6,7 @@ from contextlib import contextmanager
 
 # Create your views here.
 
-class TestView():
-    def __init__(self, request):
-        self.template_name = 'test_view.html'
-        self.request = request
-
-    def get(self, request):
-        print("self request")
-        print(self.request)
-        return render(self.request, self.template_name, {})
-
 def get_test_view(request):
     template = 'test_view.html'
     return render(request, template, {})
+
