ef5c88548e8c7000ae8f582c91b04030432f3c03
Aaron Vega <aaroncvega123@gmail.com>
2019-10-21 13:22:51 -0700

    Created models for listing uploaded class notes

--- MessageBoard/message_board_site/models.py
+++ MessageBoard/message_board_site/models.py
@@ -15,3 +15,23 @@ class MessageBoardPost(models.Model):
     content = models.CharField(max_length=1000)
     create_date = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class InstitutionType(models.Model):
+    type_name = models.CharField(max_length=1000)
+
+class Institution(models.Model):
+    name = models.CharField(max_length=1000)
+    type_id = models.ForeignKey(InstitutionType)
+
+class Instructor(models.Model):
+    name = models.CharField(max_length=1000)
+
+class Course(models.Model):
+    name = models.CharField(max_length=1000)
+    instructor = models.ForeignKey(Instructor)
+    institution = models.ForeignKey(Institution)
+
+class ClassNotes(models.Model):
+    created_by = models.ForeignKey(User)
+    title = models.CharField(max_length=1000)
+    course_id = models.ForeignKey(Course)
